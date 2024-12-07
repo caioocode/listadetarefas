6bc8fba3fa456965fb63279b30d9a7774676c2a5
caioocode <caiodev82@gmail.com>
2024-12-06 22:15:44 -0300

    aplicação finalizada... falta ajustes

---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+const button = document.querySelector('.buttonAddTask')
+const input = document.querySelector('.inputTask')
+const listaCompleta = document.querySelector('.listTasks')
+
+let minhaListaDeItens = []
+
+
+
+function adicionarNovaTarefa() {
+     // Verificar se o campo de entrada está vazio
+    if (input.value.trim() === ' '){
+    alert("Por favor, insira uma tarefa antes de adicionar!");
+    return; // Retorna para evitar adicionar uma tarefa vazia à lista
+}
+
+    
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+    input.value = '' 
+    mostrarTarefas()
+}
+
+function mostrarTarefas(){
+
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((item, posicao) => {
+        novaLi = novaLi + `
+        <li class="task ${item.concluida && "done"}">
+            
+            <img src="imagens/checkbutton.png" alt="" onclick="concluirTarefa(${posicao})">
+            
+            <p>${item.tarefa}</p>
+
+            <img src="imagens/removebutton.png" alt="" onclick="deletarItem(${posicao})">
+        </li>
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+
+}
+
+
+//deleta o item usando seu index
+function deletarItem(posicao){
+    minhaListaDeItens.splice(posicao, 1)
+    mostrarTarefas()
+}
+
+
+function concluirTarefa(posicao){
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
+}
+
+button.addEventListener('click', adicionarNovaTarefa)
+
+
+function recarregarTarefas() {
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+    if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+}
+    mostrarTarefas() 
+}   recarregarTarefas()
